38d581e54e572c3f937ac02ae4ccb37c05f45247
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-04-04 12:17:01 +1100

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let speed = 1
+        while (true) {
+            let time = 0
+            for (const pile of piles) {
+                time += Math.ceil(pile / speed)
+            }
+            if (time <= h) {
+                return speed
+            }
+
+            speed++
+        }
+    }
+}
