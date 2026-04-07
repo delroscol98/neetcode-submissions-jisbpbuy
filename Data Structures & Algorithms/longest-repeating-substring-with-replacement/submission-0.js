d68eac870ed2469362d16ac8de7f98eb800a0e20
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-04-07 20:24:15 +1000

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let count = {}
+        let l = 0
+        let r = 0
+        let res = 0
+        
+        while (r < s.length) {
+            if (count[s[r]] == undefined) {
+                count[s[r]] = 0
+            }
+            count[s[r]]++
+
+            let size = r - l + 1
+            let maxF = Math.max(...Object.values(count))
+            if (size - maxF > k) {
+                count[s[l]]--
+                size--
+                l++
+            }
+            res = Math.max(res, size)
+            r++
+        }
+
+        return res
+    }
+}
