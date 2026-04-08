0b62112b67f991e929943a39a3dffc6b09648125
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-04-08 21:59:00 +1000

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let l = 1
+        let r = Math.max(...piles)
+        let min = r
+
+        while (l <= r) {
+            let m = Math.floor((l + r) / 2)
+            let time = 0
+            for (const pile of piles) {
+                time += Math.ceil(pile / m)
+            }
+            if (time > h) {
+                l = m + 1
+            } else {
+                min = m
+                r = m - 1
+            }
+
+        }
+
+        return min
+    }
+}
