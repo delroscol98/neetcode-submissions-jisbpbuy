661856173586f7ef9e02073a96a42ee9f195a6c0
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-04-08 23:31:23 +1000

    Add: median-of-two-sorted-arrays - submission-1

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number}
+     */
+    findMedianSortedArrays(nums1, nums2) {
+        let A = nums1.length < nums2.length ? nums1 : nums2
+        let B = nums1.length >= nums2.length ? nums1 : nums2
+
+        let total = nums1.length + nums2.length
+        let half = Math.floor(total / 2)
+
+        let l = 0
+        let r = A.length - 1
+
+        while (true) {
+            let a = Math.floor((l + r) / 2)
+            let b = half - a - 2
+
+            let ALeftMax = a >= 0 ? A[a] : -Infinity
+            let ARightMin = a + 1 < A.length ? A[a + 1] : Infinity
+            let BLeftMax = b >= 0 ? B[b] : -Infinity
+            let BRightMin = b + 1 < B.length ? B[b + 1] : Infinity
+
+            if (ALeftMax <= BRightMin && ARightMin >= BLeftMax) {
+                let median = 0
+                if (total % 2 == 1) {
+                    median = Math.min(ARightMin, BRightMin)
+                } else {
+                    median = (Math.max(ALeftMax, BLeftMax) + Math.min(ARightMin, BRightMin)) / 2
+                }
+                return median
+            } else if (BLeftMax > ARightMin) {
+                l = a + 1
+            } else {
+                r = a - 1
+            }
+        }
+    }
+}
