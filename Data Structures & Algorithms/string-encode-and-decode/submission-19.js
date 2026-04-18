2c7aecb536caaee78729a61a32fe763911974589
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-04-18 21:44:07 +1000

    Add: string-encode-and-decode - submission-19

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-19.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encoded = ""
+        for (const str of strs) {
+            encoded += `${str.length}#${str}`
+        }
+        return encoded
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let res = []
+        let i = 0
+        while (i < str.length) {
+            let j = i
+
+            let length = ""
+            while (str[j] != "#") {
+                length += str[j]
+                j++
+            }
+            length = parseInt(length)
+
+            let word = ""
+            for (i = j + 1; i < j + 1 + length; i++) {
+                word += str[i]
+            }
+            res.push(word)
+        }
+        return res
+    }
+}
