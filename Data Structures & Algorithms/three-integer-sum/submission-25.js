4c70b5bb45ef0e22a0fd9977e8e622a38df8553f
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-04-22 09:05:15 +1000

    Add: three-integer-sum - submission-25

---
+++ Data Structures & Algorithms/three-integer-sum/submission-25.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums = nums.sort((a,b) => a - b)
+
+        let res = []
+        for (let l = 0; l < nums.length - 2; l++) {
+            if (nums[l] == nums[l - 1]) continue
+
+            let m = l + 1
+            let r = nums.length - 1
+            while (m < r) {
+                let sum = nums[l] + nums[m] + nums[r]
+                if (sum > 0) {
+                    r--
+                } else if (sum < 0) {
+                    m++
+                } else {
+                    res.push([nums[l], nums[m], nums[r]])
+                    m++
+                    r--
+
+                    while (m < r && nums[m] == nums[m - 1]) {
+                        m++
+                    }
+
+                    while (m < r && nums[r] == nums[r + 1]) {
+                        r--
+                    }
+                }
+            }
+        }
+
+        return res
+    }
+}
