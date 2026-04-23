9473e451b8e4ac9ac0e0b8b04a876e515b23a898
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-04-23 14:50:14 +1000

    Add: two-integer-sum - submission-22

---
+++ Data Structures & Algorithms/two-integer-sum/submission-22.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let hashmap = {}
+        for (let i = 0; i < nums.length; i++) {
+            let comp = target - nums[i]
+
+            if (hashmap[comp] == undefined) {
+                hashmap[nums[i]] = i
+            } else {
+                return [hashmap[comp], i]
+            }
+        }
+    }
+}
