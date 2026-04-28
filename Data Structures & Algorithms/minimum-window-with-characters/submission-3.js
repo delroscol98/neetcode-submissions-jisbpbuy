c989ef4177125f3b3a5ee486ade43939b0cdb32a
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-04-28 10:51:06 +1000

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.js
@@ -0,0 +1,58 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    minWindow(s, t) {
+        if (t == "") return ""
+
+        let countT = {}
+        let window = {}
+
+        let need = 0
+        for (const char of t) {
+            if (countT[char] == undefined) {
+                countT[char] = 0
+                need++
+            }
+            countT[char]++
+        }
+
+        let have = 0
+        let res = [-1, -1]
+        let resLen = Infinity
+        let l = 0
+
+        for (let r = 0; r < s.length; r++) {
+            let char = s[r]
+            if (window[char] == undefined) {
+                window[char] = 0
+            }
+            window[char]++
+
+            if (countT[char] != undefined && window[char] == countT[char]) {
+                have++
+            }
+
+            while (have == need) {
+                // update our result
+                if (r - l + 1 < resLen) {
+                    res[0] = l
+                    res[1] = r
+                    resLen = r - l + 1
+                }
+
+                //pop from the left of our window
+                window[s[l]]--
+
+                if (countT[s[l]] != undefined && window[s[l]] < countT[s[l]]) {
+                    have--
+                }
+                l++
+            }
+        }
+        let [left, right] = res
+        return s.substring(left, right + 1)
+    }
+}
