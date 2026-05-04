5f4ac22dd14c2053d614ac821d52f4793fafe6c5
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-05-04 17:37:20 +1000

    Add: is-anagram - submission-28

---
+++ Data Structures & Algorithms/is-anagram/submission-28.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) {
+            return false
+        }
+
+        let sMap = this.createMap(s)
+        let tMap = this.createMap(t)
+        let res = true
+
+        sMap.forEach((val, key) => {
+            if (tMap.get(key) == undefined || tMap.get(key) != val) {
+                res = false
+            }
+        })
+
+        return res
+    }
+
+    createMap(str) {
+        let map = new Map()
+        for (const char of str) {
+            if (!map.has(char)) {
+                map.set(char, 0)
+            }
+            map.set(char, map.get(char) + 1)
+        }
+        return map
+    }
+}
