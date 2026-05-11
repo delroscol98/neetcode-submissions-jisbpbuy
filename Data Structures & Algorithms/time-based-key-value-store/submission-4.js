34a65000c70ee297947a90e46518d02e4ad27fe5
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-05-11 12:52:47 +1000

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.js
@@ -0,0 +1,49 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if (!this.keyStore.get(key)) {
+            this.keyStore.set(key, [])
+        }
+        let arr = this.keyStore.get(key)
+        arr.push([value, timestamp])
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        if (!this.keyStore.has(key)) {
+            return ""
+        }
+
+        let arr = this.keyStore.get(key)
+        let l = 0
+        let r = arr.length - 1
+        let res = ""
+
+        while (l <= r) {
+            let m = Math.floor((l + r) / 2)
+            let [val, ts] = arr[m]
+
+            if (timestamp < ts) {
+                r = m - 1
+            } else {
+                res = val
+                l = m + 1
+            }
+        }
+
+        return res
+    }
+}
