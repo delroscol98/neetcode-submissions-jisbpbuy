78c2d7947570aaaf7d51476a5209daf276bf6a96
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-06-03 09:03:53 +1000

    Add: buy-and-sell-crypto - submission-14

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let l = 0
+        let r = 0
+        let max = 0
+        while (r < prices.length) {
+            if (prices[r] < prices[l]) {
+                l = r
+            }
+            max = Math.max(max, prices[r] - prices[l])
+            r++
+        }
+
+        return max
+    }
+}
