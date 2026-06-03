0e36907c82766e08711dba53e4c322583085f98c
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-06-03 11:58:45 +1000

    Add: minimum-window-with-characters - submission-8

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-8.js
@@ -0,0 +1,63 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    minWindow(s, t) {
+        if (s.length < t.length) return ""
+
+        let window = new Map()
+        let tCount = new Map()
+
+        let need = 0
+        for (const char of t) {
+            if (!tCount.has(char)) {
+                tCount.set(char, 0)
+                need++
+            }
+            tCount.set(char, tCount.get(char) + 1)
+        }
+
+        let have = 0
+        let res = [-1, -1]
+        let resLen = Infinity
+        let l = 0
+        let r = 0
+
+        while (r < s.length) {
+            let right = s[r]
+
+            if (!window.has(right)) {
+                window.set(right, 0)
+            }
+            window.set(right, window.get(right) + 1)
+
+            if (tCount.has(right) && tCount.get(right) == window.get(right)) {
+                have++
+            }
+
+            while (have == need) {
+                let len = r - l + 1
+                if (len < resLen) {
+                    res[0] = l
+                    res[1] = r
+                    resLen = len
+                }
+
+                let left = s[l]
+                window.set(left, window.get(left) - 1)
+
+                if (tCount.has(left) && tCount.get(left) > window.get(left)) {
+                    have--
+                }
+                
+                l++
+            }
+            r++
+        }
+
+        let [left, right] = res
+        return s.substring(left, right + 1)
+    }
+}
