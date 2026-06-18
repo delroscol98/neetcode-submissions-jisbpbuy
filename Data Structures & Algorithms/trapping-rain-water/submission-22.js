59b07608c31237b6365e04ee3f48d7f8e771e792
Collin Del Rosario <111747821+delroscol98@users.noreply.github.com>
2026-06-18 15:05:06 +1000

    Add: trapping-rain-water - submission-22

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-22.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let l = 0
+        let r = height.length -1 
+        let maxL = height[l]
+        let maxR = height[r]
+        let total = 0
+
+        while (l < r) {
+            let water = 0
+            if (maxL <= maxR) {
+                water = maxL - height[l]
+                l++
+                maxL = Math.max(maxL, height[l])
+                
+            } else {
+                water = maxR - height[r]
+                r--
+                maxR = Math.max(maxR, height[r])
+                
+            }
+            total += water
+        }
+        return total
+    }
+}
